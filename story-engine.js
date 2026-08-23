ecc0fe195de4c056b81fe4d3efd28f7a2d44855b
TtRatKb <luca_richter@yahoo.de>
2026-08-23 21:54:36 +0200

    Add files via upload

--- story-engine.js
+++ story-engine.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const PACK_URL = "content/SP_003.dat?v=0.24.0";
+  const PACK_URL = "content/SP_003.dat?v=0.24.1";
   const KEY = new TextEncoder().encode("LifeRPG-SP3-accidental-spoiler-shield-v3");
   let cache = null;
 
