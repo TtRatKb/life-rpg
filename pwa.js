36434f775b6b6e5937fa252521940cef84da69c3
TtRatKb <luca_richter@yahoo.de>
2026-08-26 15:39:49 +0200

    Add files via upload

--- pwa.js
+++ pwa.js
@@ -4,7 +4,7 @@
 
   if (!("serviceWorker" in navigator)) return;
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./service-worker.js?v=0.26.1", { scope: "./" })
+    navigator.serviceWorker.register("./service-worker.js?v=0.26.3", { scope: "./" })
       .catch(error => console.warn("Life RPG service worker could not register", error));
   });
 })();
